9af26e9a2618ed6d3bf15ffd0e1a93d166b8b875
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:38:05 +0530

    Add files via upload

---
+++ a42.c
@@ -0,0 +1,13 @@
+//program to add two integers
+#include<stdio.h>
+int main()
+{
+    int n1 , n2;
+    printf("enter the first no. = ");
+    scanf("%d",&n1);
+    printf("enter the second no. = ");
+    scanf("%d",&n2);
+    printf("The sum of the two no. is = %d",n1+n2);
+    return 0;
+}
+
