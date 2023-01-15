4ea8bfdda3481d8fdf37db78395c92856e0fff21
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:33:04 +0530

    Add files via upload

---
+++ a34.c
@@ -0,0 +1,12 @@
+//program to convert temperature in   celsius into fahrenheit
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float C;
+    printf("Enter the temperature in celsius = ");
+    scanf("%f",&C);
+    printf("The tmperature in fahrenheit is = %f ", C*(9.0/5)+32); //F=C*(9/5)+32
+    return 0;
+}
+Footer
