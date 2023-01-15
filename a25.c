7b1d2c1efa6afadc30e3dbe27831aeaa3f9fc7a6
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:26:15 +0530

    Add files via upload

---
+++ a25.c
@@ -0,0 +1,14 @@
+//program to find the sum of two numbers
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n1,n2;
+    printf(" enter n1=");
+    scanf("%d",&n1);
+     printf(" enter n2=");
+    scanf("%d",&n2);
+    printf("the sum of two numbers is = %d",n1+n2);
+    return 0;
+    
+}
