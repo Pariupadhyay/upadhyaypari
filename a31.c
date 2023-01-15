1cbfb6d10a90f9b9d01c79a788dfb26818e4960d
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:30:45 +0530

    Add files via upload

---
+++ a31.c
@@ -0,0 +1,11 @@
+//program to print integer entered by the user
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter any integer=");
+    scanf("%d",&a);
+    printf("The no. you entered is %d",a);
+    return 0;
+}
+
