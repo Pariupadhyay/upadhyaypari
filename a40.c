f66c9b3b26a83829b9dfbe83bec168b1dffef301
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:36:16 +0530

    Add files via upload

---
+++ a40.c
@@ -0,0 +1,12 @@
+//program to calculate the area of equilateral triangle
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float side;
+    printf("enter the side of traiangle is = ");
+    scanf("%f",&side);
+    printf("The area of traiangle is = %f",(1.732/4)*side*side);
+    return 0;
+}
+
