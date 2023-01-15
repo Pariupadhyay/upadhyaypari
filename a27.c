ec483bafce9b88d48ebfc0a14d748bad6fb4a2bf
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:28:05 +0530

    Add files via upload

---
+++ a27.c
@@ -0,0 +1,14 @@
+//program to calculate perimeter of rectangle
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a");
+    scanf("%d",&a);
+    printf("Enter b");
+    scanf("%d",&b);
+    printf("The perimeter of rectangle is= %d",2*(a+b));
+    return 0;
+    
+
+}
