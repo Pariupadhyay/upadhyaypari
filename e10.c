371d0b04d1e1cd179992cf4f27e17f68dd4d547f
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:45:28 +0530

    Add files via upload

---
+++ e10.c
@@ -0,0 +1,63 @@
+//program to check whter a no. is vowel or consonant using switch case
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter n");
+    scanf("%d",&n);
+
+
+    if((n>'a'&&n<'z')||(n>'A'&&n<'Z'))
+    {
+        switch(n){
+            case 'a':
+            printf("Vowel");
+            break;
+                       case 'e':
+
+            printf("Vowel");
+            break;
+                       case 'i':
+            printf("Vowel");
+            break;
+                       case 'o':
+            printf("Vowel");
+            break;
+                       case 'u':
+            printf("Vowel");
+            break;
+                       case 'A':
+            printf("Vowel");
+            break;
+                       case 'E':
+            printf("Vowel");
+            break;
+                       case 'I':
+            printf("Vowel");
+            break;
+                       case 'O':
+            printf("Vowel");
+            break;
+                       case 'U':
+            printf("Vowel");
+            break;
+            default:
+            printf("Not a Vowel");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+    
+    
+    
+    
+    }
+
