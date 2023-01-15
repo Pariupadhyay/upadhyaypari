da1007a09e3c72091569127d972b6ae52378c391
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:45:03 +0530

    Add files via upload

---
+++ e9.c
@@ -0,0 +1,34 @@
+//program to print days of week using switch case
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a= ");
+    scanf("%d",&a);
+    switch(a){
+        case 1:
+        printf("MONDAY");
+        break;
+        case 2:
+        printf("TUESDAY");
+        break;
+        case 3:
+        printf("WEDNESDAY");
+        break;
+        case 4:
+        printf("THURSDAY");
+        break;
+        case 5:
+        printf("FRIDAY");
+        break;
+        case 6:
+        printf("SATURDAY");
+        break;
+        case 7:
+        printf("SUNDAY");
+        break;
+
+
+
+    }
+}
+
