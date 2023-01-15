d00f6f8bf33a917afcb994efb784a72fd7f97765
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:30:08 +0530

    Add files via upload

---
+++ a30.c
@@ -0,0 +1,14 @@
+//program to calculate the area of rectangle
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float l,b; //l=length,b=breadth
+    printf("Enter the length of rectangle = ");
+    scanf("%f",&l);
+    printf("Enter the breadth of rectangle = ");
+    scanf("%f",&b);
+    printf("The area of rectangle is = %f ",l*b); // area=length*breadth
+    return 0;
+    
+}
