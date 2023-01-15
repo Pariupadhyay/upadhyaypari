81df29d053f4aa69e2cef5caaffc44e10ddf74dc
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:44:14 +0530

    Add files via upload

---
+++ e7.c
@@ -0,0 +1,10 @@
+//program to find maximum of two numbers
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter a = ");
+    scanf("%d",&a);
+    printf("Enter b = ");
+    scanf("%d",&b);
+    a>b?printf("a is greater"):printf("b is greater");
+}
