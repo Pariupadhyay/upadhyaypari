ced5edb249e5d11c0f2599755da3b838c6b48c23
Pariupadhyay <122715183+Pariupadhyay@users.noreply.github.com>
2023-01-15 20:22:11 +0530

    Add files via upload

---
+++ a21.cpp
@@ -0,0 +1,10 @@
+//program to calculate area of square
+#include<stdio.h>
+int main()
+{
+    int side;
+    printf("enter the side of square=");
+    scanf("%d",&side);
+    printf("the area of square is %d",side*side);
+    return 0;
+}
